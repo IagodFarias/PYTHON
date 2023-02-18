99037493a1b9622bd0d5f3f44b25cd75aff25b64
Danyelfarias <>
2023-02-18 12:11:11 -0300

    salvara

---
+++ aula10_lista/ex1.py
@@ -0,0 +1,13 @@
+value = list()
+min = max = 0
+for c in range(0,5):
+    value.append(input("digite um valor: "))
+    if 
+
+
+
+
+
+print(f'vc digitou os valores {value}')
+print(f'E o maior valor digitado foi {max(value)} na posição {}')
+print(f'E o maior valor digitado foi {min(value)} na posição {}')
---
+++ aula10_lista/lista.py
@@ -0,0 +1,2 @@
+pessoa = [['joão' ,32], ['maria', 23], ['danyel', 19]]
+print(pessoa[0][1])
